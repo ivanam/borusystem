3d5921c90d562816e99ea65f86922644a7024f78
Gaston Mura <gastonmura@hotmail.com>
2013-11-07 18:07:56 -0300

    Ultimo pedacito

--- gestiones/Mozo/urls.py
+++ gestiones/Mozo/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
                        url(r'^$', 'gestiones.Mozo.views.inicio', name="mozo"),
+                       url(r'^comanda$', 'gestiones.Mozo.views.crearcomanda', name="crearcomanda"),
 )
--- gestiones/Mozo/views.py
+++ gestiones/Mozo/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
 @permission_required('Administrador.is_mozo', login_url="login")
 def inicio(request):
-    return render_to_response('Mozo/mozo.html', {}, context_instance=RequestContext(request))
+    return render_to_response('Mozo/mozo2.html', {}, context_instance=RequestContext(request))
+
+@permission_required('Administrador.is_mozo', login_url="login")
+def crearcomanda (request):
+    print ("creamos la comanda")
+    panel_seleccionar_mesa=render_to_response('Mozo/panel_mesas_seleccionadas.html', {}, context_instance=RequestContext(request))
+    return render_to_response('Mozo/seleccionar_mesas.html', {'panel_seleccionar_mesa':panel_seleccionar_mesa}, context_instance=RequestContext(request))
+    #return HttpResponseRedirect('seleccionarmesas')
 
