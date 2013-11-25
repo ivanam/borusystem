c5ce0d6928eac0cadfa892e2a1a14e10cd3cd1bf
andresm090 <andres_m090@hotmail.com>
2013-11-25 19:50:58 -0200

    Arreglado menu ejecutivo. Parte de comanda mejorada

--- gestiones/Producto/altamenuejecutivo/forms.py
+++ gestiones/Producto/altamenuejecutivo/forms.py
@@ -16,7 +16,7 @@ class altaMenuEjecutivoForm(ModelForm):
             super(altaMenuEjecutivoForm, self).__init__(*args, **kwargs)
 
             #selecciono las secciones de la carta que son solo de platos
-            seccionesChoice = SeccionCarta.objects.filter(categoria='D')
+            seccionesChoice = SeccionCarta.objects.filter(categoria='E')
             #las formateo para crear un choice
             secciones = [(secc.id, secc.nombre) for secc in seccionesChoice]
             #indico que el widget tiene que mostrar el choice recien creado
